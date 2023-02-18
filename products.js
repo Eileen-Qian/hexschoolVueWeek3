523be57e3f7a41a28682ddfaf3e97a8d6c5a12c1
Eileen-Qian <107092198+Eileen-Qian@users.noreply.github.com>
2023-02-18 17:41:08 +0800

    Week3 finished
    新增、修改、刪除
    多圖設置

--- products.js
+++ products.js
@@ -1,12 +1,19 @@
 import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
+let productModal = null;
+let delProductModal = null;
+
 createApp({
   data() {
     return {
       api: 'https://vue3-course-api.hexschool.io/v2',
       apiPath: 'qian14',
       products: [],
-      productDetail: {}
+      productDetail: {},
+      tempProduct: {
+        imagesUrl: [],
+      },
+      isNew: false //用來確認是新增或是編輯
     };
   },
   methods: {
@@ -26,10 +33,57 @@ createApp({
         .then((response) => {
           // console.log(response.data.products);
           this.products = response.data.products;
+        }).catch((error) => {
+          alert(error.data.message);
         })
     },
     showProductDetail(productDetail) {
       this.productDetail = productDetail;
+    },
+    openModal(status, product) {
+      if (status === 'create') {
+        productModal.show();
+        this.isNew = true;
+        // 新增時會帶入初始化資料
+        this.tempProduct = {
+          imagesUrl: [],
+        };
+      } else if (status === 'edit') {
+        productModal.show();
+        this.isNew = false;
+        // 編輯的話，會帶入當前要編輯的原資料
+        this.tempProduct = { ...product };
+      } else if (status === 'delete') {
+        delProductModal.show();
+        this.tempProduct = { ...product }; // 取 id 使用
+      }
+    },
+    // 新增或編輯修改產品
+    updateProduct() {
+      let url = `${this.api}/api/${this.apiPath}/admin/product`;
+      // 用 this.isNew 判斷 API 要 POST 還是 PUT
+      let method = 'post';
+      if (!this.isNew) {
+        url = `${this.api}/api/${this.apiPath}/admin/product/${this.tempProduct.id}`;
+        method = 'put';
+      }
+      axios[method](url, { data: this.tempProduct })
+        .then(() => {
+          this.showProducts();
+          productModal.hide(); //關閉 modal        
+        })
+    },
+    deleteProduct() {
+      const url = `${this.api}/api/${this.apiPath}/admin/product/${this.tempProduct.id}`;
+
+      axios.delete(url)
+        .then(() => {
+          this.showProducts();
+          delProductModal.hide(); //關閉 modal        
+        })
+        .catch((error) => {
+          alert(error.response.data.message);
+        })
     }
   },
   mounted() {
@@ -37,5 +91,9 @@ createApp({
     // console.log(token);
     axios.defaults.headers.common['Authorization'] = token;
     this.checkLogin();
+
+    // 初始化 bootstrap modal
+    productModal = new bootstrap.Modal('#productModal');
+    delProductModal = new bootstrap.Modal('#delProductModal');
   }
 }).mount('#app');
